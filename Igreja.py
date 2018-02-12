17e23d9ef9073917d9a31a802a509105371f84aa
Lucas Vieira <tiomedianope@gmail.com>
2018-02-12 01:25:35 -0200

    Fazendo Alterações no Código

--- Igreja.py
+++ Igreja.py
@@ -1,29 +1,51 @@
-import re
+import re, os
+
+def visualizarProd():
+    itemsCon = open('produtos.txt', 'r')
+    itemsConteudo = itemsCon.read()
+    itemsList = itemsConteudo.split('\n')
+    itemsCon.close()
+    return itemsList
+
 
 while True:
     print('Digite [1] Para Cadastrar Novos Produtos')
-    print('Digite [2] Para Visualizar')
-    print('Digite [3] Para Sair')
-    entrada = input()
+    print('Digite [2] Para Visualizar Existentes')
+    print('Digite [3] Para Limpar a Tela')
+    print('Digite [4] Para Sair do Programa')
+    print('')
+    entrada = input('Digite: ')
     if entrada == '1':
+        print('')
         print('Digite o Nome do Produto')
-        nome = input()
+        nome = input('Nome Produto: ')
+        print('')
         print('Digite o Código do Produto')
-        prodCode = input()
+        prodCode = input('Código Produto: ')
+        if prodCode == '':
+            prodCode = 'Sem Código'
         items = open('produtos.txt', 'a')
-        items.write(nome + ' - ' + prodCode + '\n')
+        items.write(nome.upper() + ' - ' + prodCode + '\n')
         items.close()
+        print('')
     elif entrada == '2':
-        print('Digite o Item')
-        item = input()
-        itemsCon = open('produtos.txt', 'r')
-        itemsConteudo = itemsCon.read()
-        itemsList = itemsConteudo.split('\n')
+        print('Digite o Nome do Item ou Código')
+        item = input('Nome do Produto: ')
+        cont = 0
+        itemsList = visualizarProd()
         for i in itemsList:
-            findItem = re.search(item, i)
+            findItem = re.search(item.upper(), i)
             if findItem != None:
-                print(i)
-
-        itemsCon.close()
+                print('')
+                cont += 1
+                a = str(cont) + '- Item Encontrado '+'[ '+ i + ' ]'
+                print(''.ljust(10, '.') + ' ' + a)
+        print('')
+        if cont == 0:
+            print('Nenhum Item Encontrado no Banco de Dados')
+        else:
+            print('Total de Resultados: ' + str(cont))
     elif entrada == '3':
+        os.system('cls')
+    elif entrada == '4':
         break
